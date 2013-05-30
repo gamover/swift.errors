843918030cb69c774bf0ecf6b7db1fe70e5461dd
Smirnov Ruslan <smirnov@realweb.ru>
2013-05-30 10:52:11 +0400

    sc

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /**
- * Author: G@mOBEP
+ * Created by G@mOBEP
  *
- * Date: 20.04.13
- * Time: 16:40
+ * Email:   rusmiran@gmail.com
+ * Company: RealWeb
+ * Date:    23.05.13
+ * Time:    13:42
  */
+
+exports.MultipleError = require('./lib/multipleError').MultipleError;
+exports.SwiftError    = require('./lib/swiftError').SwiftError;
+exports.SystemError   = require('./lib/systemError').SystemError;
+exports.TypeError     = require('./lib/typeError').TypeError;
+exports.ValueError    = require('./lib/valueError').ValueError;
